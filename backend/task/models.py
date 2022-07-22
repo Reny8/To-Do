95f75eff4775e7efbe6fdb1ac92170b4716f1035
Reny Reyes <renyreyes8@hotmail.com>
2022-07-22 14:57:33 -0400

    create a task function

--- backend/task/models.py
+++ backend/task/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class Task(models.Model):
     description = models.CharField(max_length=50)
-    status = models.CharField(max_length=10)
+    status = models.CharField(max_length=15)
--- backend/task/views.py
+++ backend/task/views.py
@@ -5,10 +5,14 @@ from .models import Task
 from .serializers import TaskSerializer
 from django.shortcuts import get_object_or_404
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def task_details(request):
     if request.method == 'GET':
         tasks = Task.objects.all()
         serializer = TaskSerializer(tasks, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-        
+    elif request.method == 'POST':
+        serializer = TaskSerializer(data = request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
