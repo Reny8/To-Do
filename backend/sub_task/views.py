abaf2c06f0ed9a898b4d297e8c1515bd52bc3b50
Reny Reyes <renyreyes8@hotmail.com>
2022-07-22 15:23:05 -0400

    Function to update sub-tasks

--- backend/sub_task/views.py
+++ backend/sub_task/views.py
@@ -5,12 +5,18 @@ from .models import Sub_Task
 from .serializers import Sub_TaskSerializer
 from django.shortcuts import get_object_or_404
 
-@api_view(['GET'])
+@api_view(['GET','PUT'])
 def sub_task_details(request,pk):
     if request.method == 'GET':
         sub_tasks = Sub_Task.objects.filter(related_task__id = pk)
         serializer = Sub_TaskSerializer(sub_tasks, many= True)
         return Response(serializer.data, status= status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        sub_tasks = get_object_or_404(Sub_Task, pk = pk)
+        serializer = Sub_TaskSerializer(sub_tasks, data = request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
 
 @api_view(['POST'])
