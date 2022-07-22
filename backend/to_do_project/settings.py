f4a271571394cbc9df62333d7b289c2982d0f7c1
Reny Reyes <renyreyes8@hotmail.com>
2022-07-22 14:26:48 -0400

    Created a task app + Added to installed apps

--- backend/to_do_project/settings.py
+++ backend/to_do_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders'
+    'corsheaders',
+    'task'
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 
