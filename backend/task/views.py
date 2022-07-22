ba6f2ce7bca6f2792603b057fe4635305d5dca0c
Reny Reyes <renyreyes8@hotmail.com>
2022-07-22 14:51:32 -0400

    Added urls file in tasks + Created get all tasks function

--- backend/task/views.py
+++ backend/task/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Task
+from .serializers import TaskSerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+@api_view(['GET'])
+def task_details(request):
+    if request.method == 'GET':
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        
