479ea2c2bc861835b353e56fc191cafad60666a6
Reny Reyes <renyreyes8@hotmail.com>
2022-07-22 15:27:37 -0400

    Delete function for sub_tasks

--- backend/sub_task/views.py
+++ backend/sub_task/views.py
@@ -5,23 +5,26 @@ from .models import Sub_Task
 from .serializers import Sub_TaskSerializer
 from django.shortcuts import get_object_or_404
 
-@api_view(['GET','PUT'])
+@api_view(['GET','PUT','DELETE'])
 def sub_task_details(request,pk):
+    sub_update = get_object_or_404(Sub_Task, pk = pk)
     if request.method == 'GET':
         sub_tasks = Sub_Task.objects.filter(related_task__id = pk)
         serializer = Sub_TaskSerializer(sub_tasks, many= True)
         return Response(serializer.data, status= status.HTTP_200_OK)
     elif request.method == 'PUT':
-        sub_tasks = get_object_or_404(Sub_Task, pk = pk)
-        serializer = Sub_TaskSerializer(sub_tasks, data = request.data)
+        serializer = Sub_TaskSerializer(sub_update, data = request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
-
+    elif request.method == 'DELETE':
+        sub_update.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['POST'])
 def create_sub_task(request):
-    serializer = Sub_TaskSerializer(data = request.data)
-    if serializer.is_valid(raise_exception=True):
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    if request.method == 'POST':
+        serializer = Sub_TaskSerializer(data = request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
