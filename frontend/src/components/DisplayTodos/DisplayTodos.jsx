7698968ab942e48a668ee1577ae7b6fc8ccbc719
Reny Reyes <renyreyes8@hotmail.com>
2022-07-22 20:14:09 -0400

    Installed material ui + created display todos

--- frontend/src/components/DisplayTodos/DisplayTodos.jsx
+++ frontend/src/components/DisplayTodos/DisplayTodos.jsx
@@ -1,26 +1,37 @@
-import React from "react";
-import "./DisplayTodos.css";
+import React, { useState } from "react";
+import "./DisplayTodos.css"
+import IconButton from "@mui/material/IconButton";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import { Table, TableCell, TableRow } from "@mui/material";
 const DisplayTodos = (props) => {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="around-table">
-      <table className="table">
-        <tbody>
-          {props.todos.map((item) => {
-            return (
-              <tr scope="row" key={item.id * 2}>
-                <td>
-                  <input type="checkbox" />
-                </td>
-                <td>{item.description}</td>
-                <td>{item.status}</td>
-                <td className="around-delete">
-                  <button>&times;</button>
-                </td>
-              </tr>
+    <div className="todo-list">
+      <Table aria-lable="collapsible table">
+      
+          {props.todos.map((todo) => {
+            return (  
+            <TableRow>
+                <TableCell>
+                  {" "}
+                  <IconButton
+                    aria-label="expand row"
+                    size="small"
+                    onClick={() => setOpen(!open)}
+                  >
+                    {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                  </IconButton>
+                </TableCell>
+                <TableCell><input type="checkbox"/></TableCell>
+                <TableCell>{todo.description}</TableCell>
+                <TableCell>{todo.status}</TableCell>
+                <TableCell className="button"><button>&times;</button></TableCell>
+              </TableRow>
             );
           })}
-        </tbody>
-      </table>
+      
+      </Table>
     </div>
   );
 };
