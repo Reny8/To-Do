71994bfb54cf5ad474e97e8d2b3d7998aa8a35ea
Reny Reyes <renyreyes8@hotmail.com>
2022-07-29 21:52:57 -0400

    Changed max-length from 50 to 100

--- backend/sub_task/models.py
+++ backend/sub_task/models.py
@@ -3,5 +3,5 @@ from task.models import Task
 # Create your models here.
 class Sub_Task(models.Model):
     related_task = models.ForeignKey(Task, on_delete = models.CASCADE)
-    description = models.CharField(max_length = 50)
+    description = models.CharField(max_length = 100)
     status = models.CharField(max_length = 15)
--- backend/task/models.py
+++ backend/task/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
-    description = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
     status = models.CharField(max_length=15)
